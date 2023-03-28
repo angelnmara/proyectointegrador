0c893f0ac4d48e02469735cc6d84d33f97388b12
angelnmara <angelnmara@hotmail.com>
2023-03-27 18:55:19 -0600

    se agregan comentarios a modelo

--- src/main/java/com/lamarrulla/proyectointegrador/controller/ModelController.java
+++ src/main/java/com/lamarrulla/proyectointegrador/controller/ModelController.java
@@ -2,40 +2,74 @@ package com.lamarrulla.proyectointegrador.controller;
 
 import com.lamarrulla.proyectointegrador.dto.ModelDTO;
 import com.lamarrulla.proyectointegrador.service.ModelService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/model")
 public class ModelController {
     private final ModelService modelService;
 
     public ModelController(ModelService modelService) {
         this.modelService = modelService;
     }
-
-    @PostMapping("/model")
+    @ApiOperation( value = "Nueva modelo")
+    @ApiResponses( value = {
+            @ApiResponse( code = 200, message = "Respuesta correcta"),
+            @ApiResponse( code = 401, message = "No está autorizado para ver este recurso"),
+            @ApiResponse( code = 403, message = "Está prohibido acceder al recurso"),
+            @ApiResponse( code = 404, message = "No se encuentra el recurso")
+    })
+    @PostMapping
     public ResponseEntity<ModelDTO> saveModelo(@RequestBody ModelDTO modelDTO){
         return ResponseEntity.ok(modelService.save(modelDTO));
     }
-
-    @PutMapping("/model")
+    @ApiOperation( value = "Actiualiza modelo")
+    @ApiResponses( value = {
+            @ApiResponse( code = 200, message = "Respuesta correcta"),
+            @ApiResponse( code = 401, message = "No está autorizado para ver este recurso"),
+            @ApiResponse( code = 403, message = "Está prohibido acceder al recurso"),
+            @ApiResponse( code = 404, message = "No se encuentra el recurso")
+    })
+    @PutMapping
     public ResponseEntity<ModelDTO> updateModelo(@RequestBody ModelDTO modelDTO){
         return ResponseEntity.ok(modelService.save(modelDTO));
     }
-
-    @GetMapping("/model/{id}")
+    @ApiOperation( value = "Obtien modelo por Id")
+    @ApiResponses( value = {
+            @ApiResponse( code = 200, message = "Respuesta correcta"),
+            @ApiResponse( code = 401, message = "No está autorizado para ver este recurso"),
+            @ApiResponse( code = 403, message = "Está prohibido acceder al recurso"),
+            @ApiResponse( code = 404, message = "No se encuentra el recurso")
+    })
+    @GetMapping("/{id}")
     public ResponseEntity<ModelDTO> getModelo(@PathVariable Integer id){
         return ResponseEntity.ok(modelService.findById(id));
     }
-
-    @GetMapping("/model")
+    @ApiOperation( value = "Obtiene todos los modelos")
+    @ApiResponses( value = {
+            @ApiResponse( code = 200, message = "Respuesta correcta"),
+            @ApiResponse( code = 401, message = "No está autorizado para ver este recurso"),
+            @ApiResponse( code = 403, message = "Está prohibido acceder al recurso"),
+            @ApiResponse( code = 404, message = "No se encuentra el recurso")
+    })
+    @GetMapping
     public ResponseEntity<List<ModelDTO>> getModelo(){
         return ResponseEntity.ok(modelService.findAll());
     }
-
-    @DeleteMapping("/model/{id}")
+    @ApiOperation( value = "Borra modelo por id")
+    @ApiResponses( value = {
+            @ApiResponse( code = 200, message = "Respuesta correcta"),
+            @ApiResponse( code = 401, message = "No está autorizado para ver este recurso"),
+            @ApiResponse( code = 403, message = "Está prohibido acceder al recurso"),
+            @ApiResponse( code = 404, message = "No se encuentra el recurso")
+    })
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteModelo(@PathVariable Integer id){
         modelService.deleteById(id);
         return ResponseEntity.ok("Modelo eliminado correctamente");
