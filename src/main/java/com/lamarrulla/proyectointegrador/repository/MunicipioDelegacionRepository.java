b0bb8a5d2a2bd84a6b80ff20790b833cad189bbe
angelnmara <angelnmara@hotmail.com>
2023-05-12 19:08:00 -0600

    se quita municipio

--- src/main/java/com/lamarrulla/proyectointegrador/repository/MunicipioDelegacionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.lamarrulla.proyectointegrador.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MunicipioDelegacionRepository extends JpaRepository<MunicipioDelegacion, Integer> {
-}
